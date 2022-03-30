bffba42e93af8f05cb886707e62eb7bc21731adc
iamvictorsam <iamvictorsam@gmail.com>
2022-03-30 09:48:40 +0100

    sportalert change

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Sport Alert',
+      title: 'SportAlert',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
